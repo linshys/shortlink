6bf86e18bd0b5b6a6451b2eea32f5b91bbf238d6
shizifeng <shizifeng0615@outlook.com>
2024-02-16 13:07:12 +0800

    feature: 开发通过分布式锁完善用户注册功能，防止恶意请求短时间内使用同一用户名恶意注册

---
+++ admin/src/main/java/org/linshy/saas/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package org.linshy.saas.admin.common.constant;
+
+/**
+ * 短链接后管 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "shork-link:lock_user-register:";
+}
--- admin/src/main/java/org/linshy/saas/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/linshy/saas/admin/service/impl/UserServiceImpl.java
@@ -13,9 +13,12 @@ import org.linshy.saas.admin.dto.req.UserRegisterReqDTO;
 import org.linshy.saas.admin.dto.resp.UserRespDTO;
 import org.linshy.saas.admin.service.UserService;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import static org.linshy.saas.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 import static org.linshy.saas.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
 import static org.linshy.saas.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
@@ -27,6 +30,7 @@ import static org.linshy.saas.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERR
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -56,12 +60,25 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         {
             throw new ClientException(USER_NAME_EXIST);
         }
+        // 分布式锁防止恶意请求
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {
+                //持久层
+                int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if(insert<1)
+                    throw new ClientException(USER_SAVE_ERROR);
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+        }
+        finally {
+            lock.unlock();
+        }
 
-        int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if(insert<1)
-            throw new ClientException(USER_SAVE_ERROR);
 
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+
 
 
     }
