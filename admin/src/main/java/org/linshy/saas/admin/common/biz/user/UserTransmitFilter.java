16d4a0f85c76d8333ddb9adf9abcd7622d5ef4ec
shizifeng <shizifeng0615@outlook.com>
2024-02-26 14:21:01 +0800

    fix: 修复后管系统过滤路径

--- admin/src/main/java/org/linshy/saas/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/linshy/saas/admin/common/biz/user/UserTransmitFilter.java
@@ -1,12 +1,15 @@
 package org.linshy.saas.admin.common.biz.user;
 
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 用户信息传输过滤器
@@ -14,9 +17,18 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
+
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/user/has-username"
+    );
+    @SneakyThrows
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpServletRequest.getRequestURI();
+
+        // TODO 未知改造
         String username = httpServletRequest.getHeader("username");
         String token = httpServletRequest.getHeader("token");
         Object userInfoJsonStr = null;
