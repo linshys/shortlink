2e381e2c3584e0befe9d8f7c78079772ce535bfb
shizifeng <shizifeng0615@outlook.com>
2024-02-28 19:34:08 +0800

    feature: 用户token验证补充

--- admin/src/main/java/org/linshy/saas/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/linshy/saas/admin/common/biz/user/UserTransmitFilter.java
@@ -1,15 +1,23 @@
 package org.linshy.saas.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.linshy.saas.admin.common.convention.exception.ClientException;
+import org.linshy.saas.admin.common.convention.result.Results;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Objects;
+
+import static org.linshy.saas.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
@@ -22,30 +30,58 @@ public class UserTransmitFilter implements Filter {
             "/api/short-link/admin/v1/user/login",
             "/api/short-link/admin/v1/user/has-username"
     );
+
     @SneakyThrows
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
+        if (!IGNORE_URI.contains(requestURI)) {
+            // 判断是否是新增用户方法，不用验证token
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    return;
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr==null)
+                    {
+                        throw new ClientException(USER_TOKEN_FAIL);
+                    }
+                } catch (Exception ex) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    return;
+                }
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                userInfoDTO.setToken(token);
+                UserContext.setUser(userInfoDTO);
 
-        // TODO 未知改造
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
-        Object userInfoJsonStr = null;
-        if (username!=null && token != null)
-            userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-
-        if (userInfoJsonStr != null)
-        {
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-            userInfoDTO.setToken(token);
-            UserContext.setUser(userInfoDTO);
+            }
         }
-
         try {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
             UserContext.removeUser();
         }
     }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception {
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+        } catch (IOException e) {
+        } finally {
+            if (writer != null)
+                writer.close();
+        }
+    }
+
 }
