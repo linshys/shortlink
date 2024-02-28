883ab2e02cf548870d3613083aa108222ce0308b
shizifeng <shizifeng0615@outlook.com>
2024-02-28 13:52:48 +0800

    feature: 开发短链接t_link分库分表

--- admin/src/test/java/org/linshy/saas/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/org/linshy/saas/admin/test/UserTableShardingTest.java
@@ -1,20 +1,25 @@
 package org.linshy.saas.admin.test;
 
 public class UserTableShardingTest {
-    public static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
+    public static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
             "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(512) DEFAULT NULL COMMENT '电子邮箱',\n" +
-            "  `deletion_time` bigint(20) DEFAULT NULL COMMENT '注销时间戳',\n" +
+            "  `domain` varchar(128) DEFAULT NULL COMMENT '域名',\n" +
+            "  `short_uri` varchar(8) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '短链接',\n" +
+            "  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
+            "  `origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
+            "  `click_num` int(11) DEFAULT '0' COMMENT '点击量',\n" +
+            "  `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：未启用 1：已启用',\n" +
+            "  `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',\n" +
+            "  `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
+            "  `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
+            "  `describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_username` (`username`)\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=1758357498003922947 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';";
+            "  UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1762714317555929090 DEFAULT CHARSET=utf8mb4;";
 
     public static void main(String[] args) {
 
