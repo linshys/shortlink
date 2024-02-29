566feef462824dfce07b4168357a2bde60958f5d
shizifeng <shizifeng0615@outlook.com>
2024-02-29 14:18:12 +0800

    fix: 时间序列化修正

--- project/src/main/java/org/linshy/saas/project/dto/req/ShortLInkCreateReqDTO.java
+++ project/src/main/java/org/linshy/saas/project/dto/req/ShortLInkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package org.linshy.saas.project.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -41,6 +42,7 @@ public class ShortLInkCreateReqDTO{
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
