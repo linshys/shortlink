27a467443b0c3f861b159d3c72aa3d7b0aee39ca
shizifeng <shizifeng0615@outlook.com>
2024-03-23 21:00:40 +0900

    optimize: 优化Redis Stream的Key命名规范

--- project/src/main/java/org/linshy/saas/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/org/linshy/saas/project/common/constant/RedisKeyConstant.java
@@ -40,5 +40,15 @@ public class RedisKeyConstant {
      */
     public static final String SHORT_LINK_STATS_UIP_KEY = "short-link:stats:uip:";
 
+    /**
+     * 短链接监控消息保存队列 Topic 缓存标识
+     */
+    public static final String SHORT_LINK_STATS_STREAM_TOPIC_KEY = "short-link:stats-stream";
+
+    /**
+     * 短链接监控消息保存队列 Group 缓存标识
+     */
+    public static final String SHORT_LINK_STATS_STREAM_GROUP_KEY = "short-link:stats-stream:only-group";
+
 
 }
--- project/src/main/java/org/linshy/saas/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/org/linshy/saas/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -1,11 +1,12 @@
 package org.linshy.saas.project.mq.producer;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+
+import static org.linshy.saas.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
 
 /**
  * 短链接监控状态保存消息队列生产者
@@ -15,14 +16,11 @@ import java.util.Map;
 public class ShortLinkStatsSaveProducer {
     private final StringRedisTemplate stringRedisTemplate;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
-
     /**
      * 发送延迟消费短链接统计
      */
     public void send(Map<String, String> producerMap) {
-        stringRedisTemplate.opsForStream().add(topic, producerMap);
+        stringRedisTemplate.opsForStream().add(SHORT_LINK_STATS_STREAM_TOPIC_KEY, producerMap);
     }
 
 }
