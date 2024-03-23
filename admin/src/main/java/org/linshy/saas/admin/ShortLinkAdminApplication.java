f89ba0c66a652e096af97b675f982acc49124585
shizifeng <shizifeng0615@outlook.com>
2024-03-23 22:04:41 +0900

    refactor: 微服务改造，引入nacos

--- admin/src/main/java/org/linshy/saas/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/org/linshy/saas/admin/ShortLinkAdminApplication.java
@@ -3,7 +3,9 @@ package org.linshy.saas.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("org.linshy.saas.admin.dao.mapper")
 public class ShortLinkAdminApplication {
--- project/src/main/java/org/linshy/saas/project/ShortLinkApplication.java
+++ project/src/main/java/org/linshy/saas/project/ShortLinkApplication.java
@@ -3,7 +3,9 @@ package org.linshy.saas.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("org.linshy.saas.project.dao.mapper")
 public class ShortLinkApplication {
